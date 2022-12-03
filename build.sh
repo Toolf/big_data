a8eebea4162889d6e505907e66a9a2190c2445e1
Toolf <nazar.smirnov.2001@gmail.com>
2022-12-03 21:28:39 +0200

    fix file path

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-javac -cp /usr/local/hadoop/share/hadoop/common/hadoop-common-3.3.4.jar:/usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-client-core-3.3.4.jar:/usr/local/hadoop/share/hadoop/ common/lib/commons-cli-1.2.jar -d bin/ src/AverageCount.java
+javac -cp /usr/local/hadoop/share/hadoop/common/hadoop-common-3.3.4.jar:/usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-client-core-3.3.4.jar:/usr/local/hadoop/share/hadoop/common/lib/commons-cli-1.2.jar -d bin/ src/AverageCount.java
 
 cd bin
 jar cvf HadoopMarketAverage.jar bin
