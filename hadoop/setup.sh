e6cc14816bd23f400abd478b7110d5aa42269f97
Toolf <nazar.smirnov.2001@gmail.com>
2022-12-06 14:51:53 +0200

    Add hadoop settings

---
+++ hadoop/setup.sh
@@ -0,0 +1,6 @@
+cp ./core-site.xml /usr/local/Hadoop/etc/hadoop/core-site.xml
+cp ./hdfs-site.xml /usr/local/Hadoop/etc/hadoop/hdfs-site.xml
+cp ./mapred-site.xml /usr/local/Hadoop/etc/hadoop/mapred-site.xml
+cp ./yarn-site.xml /usr/local/Hadoop/etc/hadoop/yarn-site.xml
+cp ./masters /usr/local/Hadoop/etc/hadoop/masters
+cp ./slaves /usr/local/Hadoop/etc/hadoop/slaves
