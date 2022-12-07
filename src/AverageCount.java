2293704bc679bcb49309ef417ebf28a810b242eb
Toolf <nazar.smirnov.2001@gmail.com>
2022-12-07 21:16:41 +0200

    fix bugs

--- src/AverageCount.java
+++ src/AverageCount.java
@@ -3,6 +3,7 @@ import java.io.DataOutput;
 import java.io.IOException;
 import java.net.URI;
 import java.time.LocalDate;
+import java.util.StringTokenizer;
 import java.time.format.DateTimeFormatter;
 
 import org.apache.hadoop.conf.Configuration;
@@ -10,6 +11,7 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -17,13 +19,11 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
+
+
 public class AverageCount {
 	
-	private static final Text MARKET_KEY1=new Text("EURUSD");
-	private static final Text MARKET_KEY2=new Text("EURGBP");
-	private static final Text MARKET_KEY3=new Text("EURCHF");
-	
-	public static class AverageCountMapper extends Mapper<Object, Text, LocalDate, Double>{
+	public static class AverageCountMapper extends Mapper<Object, Text, IntWritable, Double>{
 				 
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 			String csvLine = value.toString();
@@ -33,14 +33,14 @@ public class AverageCount {
 			LocalDate year1 = LocalDate.parse(csvField[0]);
 			LocalDate year2 = LocalDate.parse(csvField[0+8]);
 
-			context.write(year1, Double.parseDouble(csvField[2]));
-			context.write(year2, Double.parseDouble(csvField[2+8]));
+			context.write(new IntWritable(year1.getYear()), Double.parseDouble(csvField[2]));
+			context.write(new IntWritable(year2.getYear()), Double.parseDouble(csvField[2+8]));
 		}
 	 }
 
-	 public static class AverageCountReducer extends Reducer<LocalDate, Double, LocalDate, Double>{
+	 public static class AverageCountReducer extends Reducer<IntWritable, Double, IntWritable, Double>{
 		 
-		public void reduce(LocalDate key, Iterable<Double> values, Context context) throws IOException, InterruptedException {
+		public void reduce(IntWritable key, Iterable<Double> values, Context context) throws IOException, InterruptedException {
 			 
 			double minValue = values.iterator().next();
 			for(Double value: values) {
@@ -73,7 +73,7 @@ public class AverageCount {
 		job.setMapperClass(AverageCountMapper.class);
 		job.setCombinerClass(AverageCountReducer.class);
 		job.setReducerClass(AverageCountReducer.class);
-		job.setOutputKeyClass(Text.class);
+		job.setOutputKeyClass(IntWritable.class);
 		job.setOutputValueClass(Double.class);
 		
 		for (int i = 1; i < otherArgs.length - 1; i++) {
