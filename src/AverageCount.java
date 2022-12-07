77684dc215c28993e14d8e9bc46849eeea3aeeb4
Toolf <nazar.smirnov.2001@gmail.com>
2022-12-07 22:14:52 +0200

    fix

--- src/AverageCount.java
+++ src/AverageCount.java
@@ -40,15 +40,15 @@ public class AverageCount {
 	 public static class AverageCountReducer extends Reducer<Text, Double, Text, Double>{
 		 
 		public void reduce(Text key, Iterable<Double> values, Context context) throws IOException, InterruptedException {
-			 
-			double minValue = values.iterator().next();
+			Double minValue = null;
 			for(Double value: values) {
-				if (minValue > value) {
+				if (minValue == null || minValue > value) {
 					minValue = value;
 				}
 			}
-
-			context.write(key, minValue);
+			if (minValue != null) {
+				context.write(key, minValue);
+			}
 		}
 
 	 }
